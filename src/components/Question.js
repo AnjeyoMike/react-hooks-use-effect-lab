d7d0a0315fc61a1dcd5c84d38b0f829d9a0dfd36
Anjeyo Mike <mike.matekeya@student.moringaschool.com>
2024-06-05 15:08:18 +0300

    Mywork

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+/*import React, { useState } from "react";
 
 function Question({ question, onAnswered }) {
-  const [timeRemaining, setTimeRemaining] = useState(10);
+  const [timeRemaining, setTimeRemaining] = useState(10);*/
 
   // add useEffect code
 
-  function handleAnswer(isCorrect) {
+  /*function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
@@ -29,4 +29,37 @@ function Question({ question, onAnswered }) {
   );
 }
 
+export default Question;*/
+
+import React, { useState, useEffect } from 'react';
+
+const Question = ({ onAnswered }) => {
+  const [timeRemaining, setTimeRemaining] = useState(10);
+
+  useEffect(() => {
+    // Set up the timeout to decrease timeRemaining by 1 every second
+    const timer = setTimeout(() => {
+      if (timeRemaining > 0) {
+        setTimeRemaining(prevTime => prevTime - 1);
+      } else {
+        setTimeRemaining(10);
+        onAnswered(false);
+      }
+    }, 1000);
+
+    // Cleanup the timeout to avoid memory leaks
+    return () => clearTimeout(timer);
+
+    // The effect depends on timeRemaining and onAnswered
+  }, [timeRemaining, onAnswered]);
+
+  return (
+    <div>
+      <p>Time Remaining: {timeRemaining}</p>
+      {/* Render the question content here */}
+    </div>
+  );
+};
+
 export default Question;
+
